8009ee1a8dc8e9b5669f21eb54926d9e62a4d713
deen26 <aaxam40@gmail.com>
2021-03-15 14:40:34 +0300

    email error display style

--- valid.js
+++ valid.js
@@ -13,3 +13,4 @@ password.addEventListener('textInput', pass_Verify);
 function validated(){
 	if (email.value.length < 9) {
 		email.style.border = "1px solid red";
+		email_error.style.display = "block";
